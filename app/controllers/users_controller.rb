84cb4962836dfe3423c5e37d7025bcc613138c50
quocbao <vuquocbao2001vp@gmail.com>
2023-06-12 13:45:00 +0700

    Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find params[:id]
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "static_pages.home.welcome"
+      redirect_to user_url @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   before_save :downcase_email
 
-  validates :name, presence: true, length: {maximum: 10}
-  validates :email, presence: true, length: {minimum: 20, maximum: 255},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6}, if: :password
+  validates :name, presence: true, length: {maximum: Settings.max_length_name}
+  validates :email, presence: true, length: {minimum: Settings.min_length_email, maximum: Settings.max_length_email},
+    format: {with: Regexp.new(Settings.valid_email_regex)}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.min_length_password}, if: :password
 
   has_secure_password
 
   private
 
   def downcase_email
-    User.email.downcase!
+    email.downcase!
   end
 end
