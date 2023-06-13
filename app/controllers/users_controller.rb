c3b309f3f59ca9a3e54e0c72c045594413c6431d
quocbao <vuquocbao2001vp@gmail.com>
2023-06-13 08:57:46 +0700

    Chapter_7 Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "flash.warning_user_not_found"
+    redirect_to home_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "flash.success_user_created"
+      redirect_to user_path @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
